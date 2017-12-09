e5742a945f9a37af383b5c8b440c78fc0c2f66b0
colonelrascals <pdhildreth@gmail.com>
2017-12-09 11:56:39 -0600

    CRUD complete

--- src/beeps/urls.py
+++ src/beeps/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import url
 
 
-from .views import BeepListView, BeepDetailView, BeepCreateView
+from .views import (
+    BeepListView, 
+    BeepDetailView, 
+    BeepCreateView, 
+    BeepUpdateView,
+    BeepDeleteView
+)
 
 urlpatterns = [
     url(r'^$', BeepListView.as_view(), name='list'),
     url(r'^create/$', BeepCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', BeepDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/edit/$', BeepUpdateView.as_view(), name='edit'),
+        url(r'^(?P<pk>\d+)/delete/$', BeepDeleteView.as_view(), name='delete'),
+
+
 ]
--- src/beeps/views.py
+++ src/beeps/views.py
@@ -1,36 +1,57 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    DetailView, 
+    ListView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
 from .models import Beep
 from .forms import BeepModelForm
-from .mixins import FormUserNeededMixin
+from .mixins import FormUserNeededMixin, UserOwnerMixin
 # Create your views here.
 
 # Create
 
 
-#Update
+# Update
 
-#Delete
+# Delete
 
 #List / Search
 
-#Retrieve
+# Retrieve
 
-class BeepCreateView(FormUserNeededMixin, CreateView):
+class BeepCreateView(LoginRequiredMixin, FormUserNeededMixin, CreateView):
     form_class = BeepModelForm
     template_name = 'beeps/create_view.html'
     success_url = '/beep/create/'
-   
+    login_url = '/admin/'
+
+
+class BeepUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    from_class = BeepModelForm
+    queryset = Beep.objects.all()
+    template_name = 'beeps/update_view.html'
+    success_url = '/beep'
+    fields = '__all__'
+
+class BeepDeleteView(LoginRequiredMixin, DeleteView):
+    model = Beep
+    template_name = 'beeps/delete_confirm.html'
+    success_url = reverse_lazy('home')
 
 
 class BeepDetailView(DetailView):
     template_name = "beeps/detail_view.html"
     queryset = Beep.objects.all()
+
     def get_object(self):
         pk = self.kwargs.get('pk')
         obj = get_object_or_404(Beep, pk=pk)
         return Beep.objects.get(id=pk)
-
 
 
 class BeepListView(ListView):
