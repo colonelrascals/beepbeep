a1cea261c16774cdcb7dd704f45c7ee88f0e6ba0
colonelrascals <pdhildreth@gmail.com>
2017-12-11 10:22:10 -0600

    bootstrap

--- src/beeps/views.py
+++ src/beeps/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -60,7 +61,11 @@ class BeepListView(ListView):
         query = self.request.GET.get('q', None)
 
         if query is not None:
-            qs= qs.filter(content__icontains=query)
+            qs= qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+            )
+
         return qs
 
     def get_context_data(self, *args, **kwargs):
