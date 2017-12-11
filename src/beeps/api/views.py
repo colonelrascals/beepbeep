5510c37c341674073645ad898b4ac566eccd9e3a
colonelrascals <pdhildreth@gmail.com>
2017-12-11 15:58:40 -0600

    hashed those tags

--- src/beeps/api/views.py
+++ src/beeps/api/views.py
@@ -7,7 +7,8 @@ from .serializers import BeepModelSerializer
 
 class BeepCreateView(generics.CreateAPIView):
     serializer_class = BeepModelSerializer
-    permission_classes= [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
@@ -15,8 +16,12 @@ class BeepCreateView(generics.CreateAPIView):
 class BeepListAPIView(generics.ListAPIView):
     serializer_class = BeepModelSerializer
     pagination_class = StandardResultsPagination
+
     def get_queryset(self, *args, **kwargs):
-        qs = Beep.objects.all().order_by('-timestamp')
+        im_following = self.request.user.profile.get_following()
+        qs1 = Beep.objects.filter(user__in=im_following)
+        qs2 = Beep.objects.filter(user=self.request.user)
+        qs = (qs1 | qs2).distinct().order_by("-timestamp")
         query = self.request.GET.get('q', None)
 
         if query is not None:
@@ -25,4 +30,19 @@ class BeepListAPIView(generics.ListAPIView):
                 Q(user__username__icontains=query)
             )
 
+        return qs
+
+
+class SearchAPIView(generics.ListAPIView):
+    serializer_class = BeepModelSerializer
+    pagination_class = StandardResultsPagination
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Beep.objects.all().order_by("-timestamp")
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+            )
         return qs
