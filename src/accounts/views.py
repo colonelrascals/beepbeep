7d15bab27f5732b645f0052f11179a56a0b6d63d
colonelrascals <pdhildreth@gmail.com>
2017-12-11 15:36:02 -0600

    user profiles

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,37 +1,38 @@
 from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import DetailView
-from django.views import View
 
+from django.views import View
+from django.views.generic import DetailView
 # Create your views here.
+
 from .models import UserProfile
 
-
 User = get_user_model()
-
 
 class UserDetailView(DetailView):
     template_name = 'accounts/user_detail.html'
     queryset = User.objects.all()
-
+    
     def get_object(self):
         return get_object_or_404(
-            User,
-            username__iexact=self.kwargs.get("username")
-        )
+                    User, 
+                    username__iexact=self.kwargs.get("username")
+                    )
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserDetailView, self).get_context_data(*args, **kwargs)
+        following = UserProfile.objects.is_following(self.request.user, self.get_object())
+        context['following'] = following
+        return context
+
 
 
 class UserFollowView(View):
     def get(self, request, username, *args, **kwargs):
         toggle_user = get_object_or_404(User, username__iexact=username)
-        if request.user.is_authenticated():
-            user_profile, created = UserProfile.objects.get_or_create(
-                user=request.user)  # (user_obj, true)
-            if toggle_user in user_profile.following.all():
-                user_profile.following.remove(toggle_user)
-            else:
-                user_profile.following.add(toggle_user)
+        if request.user.is_authenticated:
+            is_following = UserProfile.objects.toggle_follow(request.user, toggle_user)
         return redirect("profiles:detail", username=username)
         # url = reverse("profiles:detail", kwargs={"username": username})
         # HttpResponseRedirect(url)
+
